4696a07a06ea20d7340ad0ff4a75f9a67f244a36
Angela Luo <ael49021@gmail.com>
2020-05-05 21:00:37 -0500

    Write test to check that ObjectInfo constructor is correct.

--- tests/test.cc
+++ tests/test.cc
@@ -19,3 +19,9 @@ TEST_CASE("ObjectInfo updates location correctly", "[object-info]") {
   object_info.setPosition(new_position);
   REQUIRE(object_info.getPosition() == new_position);
 }
+TEST_CASE("ObjectInfo constructor stores information correctly", "[object-info][color][name]") {
+  mylibrary::ObjectInfo object_info("cool name", cinder::Color( .5f, .5f, 0.5f), cinder::vec3( 0.0f , 0.0f , 0.0f), 0);
+  cinder::Color expected_color( .5f, .5f, 0.5f);
+  REQUIRE(object_info.mName == "cool name");
+  REQUIRE(object_info.mColor== expected_color);
+}
