f3fd5a5cbfd2b795a189f66833492e297634994e
Angela Luo <ael49021@gmail.com>
2020-05-05 20:57:29 -0500

    Write test case to check that an object's location is updated correctly.

--- tests/test.cc
+++ tests/test.cc
@@ -4,6 +4,7 @@
 
 #include <cinder/Rand.h>
 #include <mylibrary/ObjectInfo.h>
+#include <mylibrary/ItemDropper.h>
 
 #include <catch2/catch.hpp>
 
@@ -11,4 +12,10 @@ TEST_CASE("Random sanity test", "[random]") {
   const float random = cinder::randFloat();
   REQUIRE(0. <= random);
   REQUIRE(random <= 1.);
+}
+TEST_CASE("ObjectInfo updates location correctly", "[object-info]") {
+  mylibrary::ObjectInfo object_info;
+  cinder::vec3 new_position (2, 3, 1);
+  object_info.setPosition(new_position);
+  REQUIRE(object_info.getPosition() == new_position);
 }
